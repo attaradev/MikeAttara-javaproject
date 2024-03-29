bd491b31d6ad4e62337503440ebb596616184a00
Mike Attara <mpyebattara@gmail.com>
2024-03-29 01:23:39 +0000

    refactor

--- src/main/java/dev/attara/stockify/StockifyApplication.java
+++ src/main/java/dev/attara/stockify/StockifyApplication.java
@@ -3,9 +3,17 @@ package dev.attara.stockify;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * The main class to start the Stockify application.
+ */
 @SpringBootApplication
 public class StockifyApplication {
 
+	/**
+	 * The main method to start the Stockify application.
+	 *
+	 * @param args The command-line arguments.
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(StockifyApplication.class, args);
 	}
